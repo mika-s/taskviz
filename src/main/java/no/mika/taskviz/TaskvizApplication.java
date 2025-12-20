77507803076478b5d478cd58d76aa7c46af36a18
Mika Sundland <mika.sundland@gmail.com>
2025-12-20 23:44:58 +0100

    Initial commit: Set up Spring Boot with necessary dependencies, and a simple Open Street Map implementation

---
+++ src/main/java/no/mika/taskviz/TaskvizApplication.java
@@ -0,0 +1,13 @@
+package no.mika.taskviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskvizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskvizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/mika/taskviz/TaskvizApplicationTests.java
@@ -0,0 +1,13 @@
+package no.mika.taskviz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskvizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
